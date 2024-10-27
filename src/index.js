9c880604b9008c3ce48a888092a83406edddc123
sheikh4200 <159758352+sheikh4200@users.noreply.github.com>
2024-10-27 16:28:59 +0500

    connect to mongobd

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
 dotenv.config({
-  path: './.env',
+  path: '.env',
 })
 
 connectDB()
-
+.then(()=>{
+  app.on((err)=>{
+console.log("Error to run the code",err)
+throw err
+  })
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`Server is running on port : ${process.env.PORT}`)
+  })
+})
+.catch((err)=>{
+console.log("failed To Connect DB !!!")
+throw err
+})
 
 
 
